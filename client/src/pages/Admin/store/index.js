21576dc2c20be2308e35b86eb487cab7d2413b5e
HemanthA0921 <hemanth.a21@iiits.in>
2024-05-02 00:20:30 +0530

    modified

--- client/src/pages/Admin/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
-import thunk from 'redux-thunk';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
