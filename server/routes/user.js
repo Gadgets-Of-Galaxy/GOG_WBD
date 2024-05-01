7b326b317eedbd96028b6d5a76ede7bc0337c74e
HemanthA0921 <97177861+HemanthA0921@users.noreply.github.com>
2024-05-02 01:37:30 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -40,7 +40,7 @@ router.post('/register', async (req, res) => {
 
         await newUser.save();
         // const token = jwt.sign({ email: newUser.email, userId: newUser._id }, JWT_SECRET_KEY);
-        res.status(201).json({ message: 'User created successfully', token });
+        res.status(201).json({ message: 'User created successfully' });
         console.log("Successfully created");
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
